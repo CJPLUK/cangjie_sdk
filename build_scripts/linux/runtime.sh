6c6a248c319d3f34aa9f6910d109019da98823f3
hw-erl-lnaylor <luke.naylor@h-partners.com>
2026-04-22 14:03:05 +0000

    Use rsync instead of cp for recursive copies

--- build_scripts/linux/runtime.sh
+++ build_scripts/linux/runtime.sh
@@ -10,5 +10,6 @@ cd $WORKSPACE/cangjie_runtime/runtime;
 [ "$SKIP_CLEAN" -eq 1 ] || python3 build.py clean;
 python3 build.py build -t $TARGET -v ${CANGJIE_VERSION};
 python3 build.py install;
-cp -R $WORKSPACE/cangjie_runtime/runtime/output/common/linux_${TARGET}_${ARCH}/{lib,runtime} $WORKSPACE/cangjie_compiler/output;
+rsync -a $WORKSPACE/cangjie_runtime/runtime/output/common/linux_${TARGET}_${ARCH}/lib/ $WORKSPACE/cangjie_compiler/output/lib/;
+rsync -a $WORKSPACE/cangjie_runtime/runtime/output/common/linux_${TARGET}_${ARCH}/runtime/ $WORKSPACE/cangjie_compiler/output/runtime/;
 
--- build_scripts/linux/stdlib.sh
+++ build_scripts/linux/stdlib.sh
@@ -9,5 +9,5 @@ cd $WORKSPACE/cangjie_runtime/stdlib;
 [ "$SKIP_CLEAN" -eq 1 ] || python3 build.py clean;
 python3 build.py build -t release --target-lib=$WORKSPACE/cangjie_runtime/runtime/output
 python3 build.py install;
-cp -R $WORKSPACE/cangjie_runtime/stdlib/output/* $WORKSPACE/cangjie_compiler/output/;
+rsync -a $WORKSPACE/cangjie_runtime/stdlib/output/ $WORKSPACE/cangjie_compiler/output/;
 
